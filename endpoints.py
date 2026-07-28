10321eb4a5be293ce872eed0005338d5f0076b99
M.Tayyab <muhammadtayyab.se@gmail.com>
2026-07-29 01:39:36 +0500

    unnecessary endpoints removed

--- endpoints.py
+++ endpoints.py
@@ -37,30 +37,6 @@ ENDPOINTS = [
 
     },
     
-    {
-        "path": "/markets/trends/growth-rates", 
-        "table": "market_trends_growth_rates",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    
-    },
-
-    {
-        "path": "/markets/trends/years-to-own", 
-        "table": "market_trends_years_to_own",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
 
     {
         "path": "/markets/trends/demand-profile", 
@@ -100,99 +76,19 @@ ENDPOINTS = [
         "on_conflict": ["area_id","period_end","property_type"]
 
     },
-
+        
     {
-        "path": "/markets/supply/ls", 
-        "table": "market_supply_ls",
+        "path": "/markets/trends/days-on-market", 
+        "table": "market_supply",
         "params":{ 
             "limit": "1000",
             "offset": "0",
             "area_id":[],
             "level": "suburb"
-        },   
+        },     
         "on_conflict": ["area_id","period_end","property_type"]
-    },
 
-    {
-        "path": "/markets/supply/ss", 
-        "table": "market_supply_ss",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },   
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
+    }
 
-
-    {
-        "path": "/markets/scores", 
-        "table": "market_scores",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
-
-    {
-        "path": "/markets/risk", 
-        "table": "market_risk",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
-
-    {
-        "path": "/markets/fundamentals", 
-        "table": "market_fundamentals",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
-
-
-    {
-        "path": "/markets/cycle", 
-        "table": "market_cycle",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
-
-    {
-        "path": "/markets/growth/cumulative", 
-        "table": "market_growth_cumulative",
-        "params":{ 
-            "limit": "1000",
-            "offset": "0",
-            "area_id":[],
-            "level": "suburb"
-        },
-        "on_conflict": ["area_id","period_end","property_type"]
-    },
-
-
-# Post Requests
-    # {
-    #     "path": "/markets/rank", 
-    #     "table": "market_rank"
-
-    # },
 
 ]
